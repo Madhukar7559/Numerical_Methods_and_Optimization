a2ba6b016d93c0ec0e8607affdd6adb51e8a2b39
Sai Madhukar Raghupatruni <91310234+Madhukar7559@users.noreply.github.com>
2021-12-28 23:12:56 +0530

    Updated boundary.cpp

--- Optimization/Boundary.cpp
+++ Optimization/Boundary.cpp
@@ -9,7 +9,7 @@ static int k=0;
 int main(void)
 {
     double x, del, y=0, w=0;// w == f(x-1), y == f(x+1), x == f(x)
-    cout << "Enter initial guess and increment" << endl; 
+    cout << "Enter initial guess and increment : "; 
     cin >> x >> del;
     if( (eq(x - mod(del)) > eq(x) || eq(x - mod(del)) == eq(x)) && ( eq(x) > eq(x + mod(del)) || eq(x) == eq(x + mod(del)) ))
     {
@@ -24,6 +24,8 @@ int main(void)
         cout << "Re-enter" << endl;
         main();
     }
+    cout << endl;
+    cout << "(" << w << ", " << y << ")" << endl;
     y = x + (twos(k)*del);
     while(eq(y) < eq(x))
     {
@@ -31,8 +33,12 @@ int main(void)
         w=x;
         x=y;
         y = x + (twos(k)*del);
+        cout << "(" << w << ", " << y << ")" << endl;
     }
-    cout << w << "\t" << y << endl;
+    cout << endl;
+    cout << "The interval in which the minimum point lie : ";
+    cout << "(" << w << ", " << y << ")" << endl;
+    cout << endl;
 }
 inline double mod(double x)
 {
@@ -40,9 +46,9 @@ inline double mod(double x)
 }
 inline double eq(double x)
 {
-	return pow(x,2) + (54/x);
+	return 0.1*pow(pow(x,2)-(3*x)+5,2) + pow(x-3,2);
 }
-inline double twos(double x)
+double twos(double x)
 {
     int p=1;
     if(x != 0)
@@ -57,5 +63,4 @@ inline double twos(double x)
     {
         return 1;
     }
-
 }
