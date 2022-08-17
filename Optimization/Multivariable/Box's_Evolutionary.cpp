be5c62e1092cdea10f6b2cdf46bc97d4257ade60
Sai Madhukar Raghupatruni <91310234+Madhukar7559@users.noreply.github.com>
2022-08-17 17:57:49 +0530

    Update Box's_Evolutionary.cpp

--- Optimization/Multivariable/Box's_Evolutionary.cpp
+++ Optimization/Multivariable/Box's_Evolutionary.cpp
@@ -20,7 +20,7 @@ int main()
         x[3][0] = x[0][0] - (delta[0]/2); x[3][1] = x[0][1] + (delta[1]/2);
         x[4][0] = x[0][0] - (delta[0]/2); x[4][1] = x[0][1] - (delta[1]/2);
         order(x,delta);
-		cout << "Minimum point : " << setprecision(10) << x[0][0] << " " << x[0][1] << endl;
+	cout << "Minimum point : " << setprecision(10) << x[0][0] << " " << x[0][1] << endl;
         cout << setprecision(10) << magnitude(delta[0], delta[1]) << endl;
     }
     cout << endl;
