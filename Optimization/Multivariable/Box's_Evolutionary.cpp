983bade97b6a4c59581448c210cb8601ccbc77e4
Sai Madhukar Raghupatruni <91310234+Madhukar7559@users.noreply.github.com>
2022-08-17 17:57:33 +0530

    Update Box's_Evolutionary.cpp

--- Optimization/Multivariable/Box's_Evolutionary.cpp
+++ Optimization/Multivariable/Box's_Evolutionary.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 double eq(double, double);
 double magnitude(double, double);
-void order(void);
-double x[5][2], delta[2];
+void order(double (*)[2], double *);
 int main()
 {
+    double x[5][2], delta[2];
     double error = 0.001;
     cout << "Enter initial guess : ";
     cin >> x[0][0] >> x[0][1];
@@ -16,14 +16,16 @@ int main()
     while(magnitude(delta[0], delta[1]) > error)
     {
         x[1][0] = x[0][0] + (delta[0]/2); x[1][1] = x[0][1] + (delta[1]/2);
-        x[2][0] = x[0][0] + (delta[0]/2); x[2][1] = x[0][1] - (delta[1]/2);
+        x[2][0] = x[0][0] + (delta[0]/2); x[2][1] = x[0][1] - (delta[1]/2);6 6
         x[3][0] = x[0][0] - (delta[0]/2); x[3][1] = x[0][1] + (delta[1]/2);
         x[4][0] = x[0][0] - (delta[0]/2); x[4][1] = x[0][1] - (delta[1]/2);
-        order();
-	cout << "Minimum point : " << setprecision(10) << x[0][0] << " " << x[0][1] << endl;
+        order(x,delta);
+		cout << "Minimum point : " << setprecision(10) << x[0][0] << " " << x[0][1] << endl;
         cout << setprecision(10) << magnitude(delta[0], delta[1]) << endl;
     }
-    cout << "Minimum point : " << setprecision(10) << x[0][0] << "\t" << x[0][1] << endl;
+    cout << endl;
+    cout << "Minimum point is " << setprecision(10) << x[0][0] << " " << x[0][1] << endl;
+    cout << endl;
     return 0;
 }
 inline double eq(double x, double y)
@@ -34,7 +36,7 @@ inline double magnitude(double x, double y)
 {
     return sqrt(pow(x,2) + pow(y,2));
 }
-void order(void)
+void order(double x[5][2], double delta[])
 {
     double t=eq(x[0][0], x[0][1]), xnew[2];
     for (int i = 0; i <= 4; i++)
@@ -46,14 +48,12 @@ void order(void)
             xnew[1] = x[i][1];
         }
     }
-    // checking if x0 == xnew
     if(t == eq(x[0][0], x[0][1]))
     {
         for (int i = 0; i < 2; i++)
         {
             delta[i] = (delta[i]/2);
         }
-        
     }
     else
     {
