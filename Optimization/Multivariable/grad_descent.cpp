2dacff70175cedc85d86ca803dd86a42d9a847e0
Sai Madhukar Raghupatruni <91310234+Madhukar7559@users.noreply.github.com>
2025-06-15 20:08:45 +0000

    Update grad_descent.cpp
    stopping condition change

--- Optimization/Multivariable/grad_descent.cpp
+++ Optimization/Multivariable/grad_descent.cpp
@@ -12,14 +12,14 @@ int main()
 {
 	double x, y, alpha;
 	cout << "Enter Initial Guess x y : ";
-	cin >> x >> y;
+	cin >> x >> y; // model parameters
 	cout << "alpha time : ";
-	cin >> alpha;
+	cin >> alpha; //learning rate
 	cout << endl;
 	int i = 0;
 	double xnew = 0;
 	double ynew = 0;
-	double error = 0.001;
+	double error = 0.001; //stopping error critera
 	double xprev;
 	double yprev;
 	do
@@ -27,14 +27,14 @@ int main()
 		xprev = x;
 		yprev = y;
 		i++;
-		cout << "iteration = " << i << endl;
+		//cout << "iteration = " << i << endl;
 		xnew = x - (alpha * partial_x(x, y));
 		ynew = y - (alpha * partial_y(x, y));
 		x = xnew;
 		y = ynew;
 		cout << setprecision(10) << "(" << x << ", " << y << ")" << endl;
 	}
-	while ((mod(xprev) - mod(x)) > error && (mod(yprev) - mod(y)) > error);
+	while (mod(eq(x,y) - eq(xprev, yprev)) > error) // magnitude of gradient > error is also a stopping condition idk why? but gotta know why?
 	cout << endl;
 	cout << "Total Iterations : " << i << endl; 
 	cout << setprecision(10) << "Final optimal = (" << x << ", " << y << ")" << endl;
@@ -49,12 +49,12 @@ double eq(double x, double y)
 
 double partial_x(double x, double y)
 {
-	return (eq(x + 0.001, y) - eq(x - 0.001, y))/(2*0.001);
+	return (eq(x + 0.001, y) - eq(x - 0.001, y))/(2*0.001); // del == h == 0.001
 }
 
 double partial_y(double x, double y)
 {
-	return (eq(x, y + 0.001) - eq(x, y - 0.001))/(2*0.001);
+	return (eq(x, y + 0.001) - eq(x, y - 0.001))/(2*0.001); // del == h == 0.001
 }
 
 double mod(double x)
